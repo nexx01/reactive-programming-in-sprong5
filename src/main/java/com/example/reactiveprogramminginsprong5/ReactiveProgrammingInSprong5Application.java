9778050dee80d3f8ef141e553ff6bae8f52cb72a
Алексей Ашутов <i.ashutov@yandex.ru>
2023-01-28 20:35:05 +0300

    -init

---
+++ src/main/java/com/example/reactiveprogramminginsprong5/ReactiveProgrammingInSprong5Application.java
@@ -0,0 +1,13 @@
+package com.example.reactiveprogramminginsprong5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgrammingInSprong5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveProgrammingInSprong5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactiveprogramminginsprong5/ReactiveProgrammingInSprong5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiveprogramminginsprong5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProgrammingInSprong5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
