c43a04ec89a3b43fec79cc160b5497618b8bbed0
Алексей Ашутов <i.ashutov@yandex.ru>
2023-02-13 19:26:53 +0300

    -init

---
+++ jdbc-01/src/test/java/com/example/jdbc01/Jdbc01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbc01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdbc01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
